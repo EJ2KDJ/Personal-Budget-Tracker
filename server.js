ece3dde7ae53b3967def190fd678383aec6e25f2
ej2kdj <emmanuelle.salga2007@gmail.com>
2024-11-29 16:43:53 +0800

    Created envelope route

--- server.js
+++ server.js
@@ -13,6 +13,41 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 
+// envelopes route
+const envelope = [];
+
+// Api create envelop
+app.use('/api/envelop', (req, res, next) => {
+    const {title, budget} = req.body;
+
+    if (!title || !budget) {
+        res.status(400).json({
+            error: "Title and Budget required" 
+        });
+    }
+
+    // new envelop
+    const newEnvelop = {
+        id: envelop + 1,
+        title: title,
+        budget: budget
+    };
+
+    // push to envelope array
+    envelop.push(newEnvelop);
+
+    res.status(201).json({
+        message: 'Envelop created',
+        envelop: envelope
+    });
+});
+
+
+// Get all envelopes
+app.get('/api/envelope', (req, res, next) => {
+    res.json(envelope);
+});
+
 // Get request hello
 app.use("/",(req, res, next) => {
     res.send("Hello World");
