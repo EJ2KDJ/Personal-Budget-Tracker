e530b576294dba703454e0254dabe357011d6254
ej2kdj <emmanuelle.salga2007@gmail.com>
2024-12-06 19:20:15 +0800

    Project completion yey!

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-// Envelopes route
+// Envelopes route with preset envelopes for example
 const envelope = [
     {
         id: 1,
